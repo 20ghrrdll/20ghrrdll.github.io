6c8f929d177a3c9543201fd91ad4ebca5cf2d938
Carah Alexander <alexander.carah@gmail.com>
2024-06-05 17:25:59 -0400

    updated css for nav, blog entry card

--- src/components/Blog-Entry-Card.js
+++ src/components/Blog-Entry-Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card'
+import { NavLink } from 'react-router-dom';
 import Tag from './Tag';
 import './Card.css'
 import './Blog-Entry-Card.css'
@@ -7,12 +7,18 @@ import './Blog-Entry-Card.css'
 const BlogEntryCard = ({id, title, date, description, tags}) => {
   const titleLink = `/blog/post/${id}`
   return (
-      <Card title={title} date={date} titleLink={titleLink}>
+    <div className="blog-entry-card">
+      <div className="card-body">
+        <NavLink to={titleLink} className="card-link">
+          <h2 className={`card-title`}>{title}</h2> 
+        </NavLink>
+        <h4> {date}</h4>
         <p>{description}</p>
-        <div className= "tags-container">
-          {tags.map(tag => <Tag tagName={tag} key={`${id}+${tag}`}/>)}
-        </div>
-      </Card>
+      <div className= "tag-container">
+        {tags.map(tag => <Tag tagName={tag} key={`${id}+${tag}`}/>)}
+      </div>
+      </div>
+    </div>
   );
 }
 
